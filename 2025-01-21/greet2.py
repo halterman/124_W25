052c338b2ef7e0db8cc201cba401d20943a8e003
Rick Halterman <halterman@southern.edu>
2025-01-23 11:55:11 -0500

    Version: Thu Jan 23 11:55:10 EST 2025

--- 2025-01-21/greet2.py
+++ 2025-01-21/greet2.py
@@ -2,4 +2,5 @@ name = input('Please enter your name: ')
 print('Pleased to meet you,', name)
 
 age = int(input("please enter your age: "))
+print('***** age =', age)
 print(age + 100)
---
+++ 2025-01-23/selectedprinting.py
@@ -0,0 +1,26 @@
+# print(1, end='***')
+# print(2, end='***')
+# print(3, end='***')
+# print(4, end='***')
+# print(5)
+
+# print(10, 20, 30, 40, 50, sep=', ')
+
+# name = input("Please enter your name: ")
+# age = int(input("Please enter your age: "))
+# # This puts a space before the period.
+# #print('Nice to meet you,', name, 'of age', age, '.')
+
+# # This works, but I would like to have only one print.
+# # print('Nice to meet you,', name, 'of age', age, end='')
+# # print('.')
+
+# print(f'Very nice to meet you {name}, of age {age + 100}.')
+
+print(f'{1:9}')
+print(f'{10:9}')
+print(f'{100:9}')
+print(f'{1000:9}')
+print(f'{10000:9}')
+print(f'{100000:9}')
+
